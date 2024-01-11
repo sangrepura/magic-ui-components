55162a28f0775f9618fcd886b601f66d4a96aed3
Arghya Das <arghyadasproject@gmail.com>
2024-01-11 23:26:01 +0530

    text shimmer effect

--- src/App.jsx
+++ src/App.jsx
@@ -21,6 +21,11 @@ export default function App() {
           End for now 🎉
         </h2>
       </div>
+      <div className="h-[50vh] bg-neutral-900">
+        <h1 className="shimmer font-normal text-2xl md:text-3xl lg:text-4xl xl:text-5xl flex items-center h-[50vh] text-neutral-500/20 justify-center">
+          Press D to get the shimmer effect
+        </h1>
+      </div>
     </main>
   );
 }
