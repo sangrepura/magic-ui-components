862015513eda42d510fde24e2938dea66cd04d94
Arghya Das <arghyadasproject@gmail.com>
2024-01-11 21:18:45 +0530

    first text reveal animation on scroll

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import TextRevealByChar from "./assets/components/TextRevealByChar";
+import TextRevealByWord from "./assets/components/TextrevealByword";
+export default function App() {
+  return (
+    <main>
+      <div className="h-[50vh] bg-red-500">
+        <h2 className="font-bold text-2xl md:text-3xl lg:text-4xl xl:text-5xl flex items-center h-[50vh] justify-center">
+          Text Reveal By Character 👇
+        </h2>
+      </div>
+      <TextRevealByChar />
+      <div className="h-[50vh] bg-red-500">
+        <h2 className="font-bold text-2xl md:text-3xl lg:text-4xl xl:text-5xl flex items-center h-[50vh] justify-center">
+          Text Reveal By Word 👇
+        </h2>
+      </div>
+      <TextRevealByWord />
+      <div className="h-[50vh] bg-red-500"></div>
+    </main>
+  );
+}
---
+++ src/assets/components/TextRevealByChar.jsx
@@ -0,0 +1,76 @@
+import React from "react";
+import { useInView, motion, useScroll, useTransform } from "framer-motion";
+import { useRef, useEffect } from "react";
+
+export default function TextRevealByChar() {
+  const ref = useRef(null);
+  const isInView = useInView(ref);
+
+  const targetRef = useRef(null);
+  const { scrollYProgress } = useScroll({
+    target: targetRef,
+  });
+  useEffect(() => {
+    scrollYProgress.on("change", (v) => {
+      console.log(v);
+    });
+  }, [scrollYProgress]);
+
+  const text =
+    "This is scroll by Characters.In Chronicle everything is made with Blocks that come with pixel perfect design, interactivity and motion out of the box. Instead of designing from scratch, simply choose the right one from our library of blocks and see the magic unfold.";
+  const words = text.split(" ");
+
+  return (
+    <main>
+      <div ref={targetRef} className="relative z-0 h-[200vh]">
+        <div className="sticky top-0 py-[5rem] px-0 flex items-center h-[50%] text-white/20 bg-neutral-900">
+          <div className="max-w-4xl mx-auto px-[1rem]">
+            <p
+              ref={targetRef}
+              className="paragraph flex flex-wrap text-2xl md:text-3xl lg:text-4xl xl:text-5xl font-bold p-[40px]"
+            >
+              {words.map((word, i) => {
+                const start = i / words.length;
+                const end = start + 1 / words.length;
+                return (
+                  <Word key={i} progress={scrollYProgress} range={[start, end]}>
+                    {word}
+                  </Word>
+                );
+              })}
+            </p>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
+}
+const Word = ({ children, progress, range }) => {
+  const amount = range[1] - range[0];
+  const step = amount / children.length;
+  return (
+    <span className="word relative mx-1 lg:mx-2.5 xl:lg-3">
+      {children.split("").map((char, i) => {
+        const start = range[0] + i * step;
+        const end = range[0] + (i + 1) * step;
+        return (
+          <Char key={`c_${i}`} progress={progress} range={[start, end]}>
+            {char}
+          </Char>
+        );
+      })}
+    </span>
+  );
+};
+
+const Char = ({ children, progress, range }) => {
+  const opacity = useTransform(progress, range, [0, 1], [0.2, 1]);
+  return (
+    <span>
+      <span className="shadow absolute opacity-30">{children}</span>
+      <motion.span style={{ opacity: opacity }} className="text-white">
+        {children}
+      </motion.span>
+    </span>
+  );
+};
