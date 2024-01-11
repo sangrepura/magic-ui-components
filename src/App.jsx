4eb5f9811cd4757c0a2b622bd45c2e23ab01b4cc
Arghya Das <arghyadasproject@gmail.com>
2024-01-11 21:54:46 +0530

    first text reveal animation container color change

--- src/App.jsx
+++ src/App.jsx
@@ -4,21 +4,21 @@ import TextRevealByWord from "./components/text-reveal/TextRevealWord";
 export default function App() {
   return (
     <main>
-      <div className="h-[50vh] bg-neutral-500">
+      <div className="h-[50vh] bg-amber-500">
         <h2 className="font-bold text-2xl md:text-3xl lg:text-4xl xl:text-5xl flex items-center h-[50vh] justify-center">
           Text Reveal By Character 👇
         </h2>
       </div>
       <TextRevealByChar />
-      <div className="h-[50vh] bg-neutral-500">
+      <div className="h-[50vh] bg-red-500">
         <h2 className="font-bold text-2xl md:text-3xl lg:text-4xl xl:text-5xl flex items-center h-[50vh] justify-center">
           Text Reveal By Word 👇
         </h2>
       </div>
       <TextRevealByWord />
-      <div className="h-[50vh] bg-neutral-500">
+      <div className="h-[50vh] bg-lime-500">
         <h2 className="font-bold text-2xl md:text-3xl lg:text-4xl xl:text-5xl flex items-center h-[50vh] justify-center">
-          End 🎉
+          End for now 🎉
         </h2>
       </div>
     </main>
