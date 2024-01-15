0ff7f42b1848c00d88d8a6c16108da2813182d3b
Arghya Das <arghyadasproject@gmail.com>
2024-01-15 20:20:43 +0530

    text-reveal component with different class props

--- src/App.jsx
+++ src/App.jsx
@@ -1,52 +1,70 @@
 import React from "react";
 import TextRevealByChar from "./components/text-reveal/TextRevealCharacter";
 import TextRevealByWord from "./components/text-reveal/TextRevealWord";
-import TextRevealByWordComponent from "./components/text-reveal/TextPrevealProd";
 import TextShimmer from "./components/text-shimmer/TextShimmer";
 import TextRevealComponent from "./components/text-reveal-parts/TextRevealComponent";
 export default function App() {
   return (
     <main>
+      {/* ====================================================================================================== */}
+
       <div className="h-[50vh] bg-amber-500">
         <h2 className="font-bold text-2xl md:text-3xl lg:text-4xl xl:text-5xl flex items-center h-[50vh] justify-center">
           Text Reveal By Character 👇
         </h2>
       </div>
+
+      {/* ====================================================================================================== */}
+
       <TextRevealByChar />
+
+      {/* ====================================================================================================== */}
+
       <div className="h-[50vh] bg-red-500">
         <h2 className="font-bold text-2xl md:text-3xl lg:text-4xl xl:text-5xl flex items-center h-[50vh] justify-center">
-          Text Reveal By Word 👇
+          Text Reveal By Word [re-usable]👇
         </h2>
       </div>
+
+      {/* ====================================================================================================== */}
+
       <TextRevealByWord text="This is scroll by words. In Chronicle everything is made with Blocks that come with pixel perfect design, interactivity and motion out of the box. Instead of designing from scratch, simply choose the right one from our library of blocks and see the magic unfold." />
+
+      {/* ====================================================================================================== */}
+
       <div className="h-[50vh] bg-lime-500">
         <h2 className="font-bold text-2xl md:text-3xl lg:text-4xl xl:text-5xl flex items-center h-[50vh] justify-center">
           Text shimmer component 👇
         </h2>
       </div>
+
+      {/* ====================================================================================================== */}
+
       <div className="h-[50vh] flex items-center justify-center bg-neutral-900">
         <TextShimmer text="Press D to get the shimmer effect" />
       </div>
-      <div className="h-[50vh] bg-lime-500">
-        <h2 className="font-bold text-2xl md:text-3xl lg:text-4xl xl:text-5xl flex items-center h-[50vh] justify-center">
-          Re-usable component 👇
-        </h2>
-      </div>
-      <TextRevealByWordComponent
-        stickyClassName="bg-red-500"
-        text="This is scroll by words. In Chronicle everything is made with Blocks that come with pixel perfect design, interactivity and motion out of the box. Instead of designing from scratch, simply choose the right one from our library of blocks and see the magic unfold."
-      />
+
+      {/* ====================================================================================================== */}
+
       <div className="h-[50vh] bg-lime-500">
         <h2 className="font-bold text-2xl md:text-3xl lg:text-4xl xl:text-5xl flex items-center h-[50vh] justify-center">
           Shadcn style 👇
         </h2>
       </div>
+
+      {/* ====================================================================================================== */}
+
       <TextRevealComponent text="This is scroll by words. In Chronicle everything is made with Blocks that come with pixel perfect design, interactivity and motion out of the box. Instead of designing from scratch, simply choose the right one from our library of blocks and see the magic unfold." />
+
+      {/* ====================================================================================================== */}
+
       <div className="h-[50vh] bg-lime-500">
         <h2 className="font-bold text-2xl md:text-3xl lg:text-4xl xl:text-5xl flex items-center h-[50vh] justify-center">
           End for now 🎉
         </h2>
       </div>
+
+      {/* ====================================================================================================== */}
     </main>
   );
 }
--- src/components/text-reveal/TextRevealWord.tsx
+++ src/components/text-reveal/TextRevealWord.tsx
@@ -1,157 +1,107 @@
-import React, { FC, ReactNode, useRef } from "react";
 import { motion, useScroll, useTransform } from "framer-motion";
+import React, { FC, ReactNode, useRef } from "react";
 import { cn } from "../../../lib/utils";
 
-interface TextRevealHeightContainerProps {
-  targetRef: React.MutableRefObject<HTMLDivElement | null>;
-  children?: React.ReactNode;
-  className?: string;
-}
-
-const TextRevealHeightContainer: FC<TextRevealHeightContainerProps> = ({
-  targetRef,
-  children,
-  className,
-}) => {
-  return (
-    <div ref={targetRef} className={cn("h-[200vh] relative z-0", className)}>
-      {children}
-    </div>
-  );
-};
-
-// This component is used to create a sticky container that will hold the text
-interface TextRevealStickyContainerProps {
-  children?: React.ReactNode;
-  className?: string;
-}
-
-const TextRevealStickyContainer: FC<TextRevealStickyContainerProps> = ({
-  children,
-  className,
-}) => {
-  return (
-    <div
-      className={cn(
-        "sticky top-0 py-[5rem] px-0 flex items-center h-[50%] text-white/20 bg-neutral-900",
-        className
-      )}
-    >
-      {children}
-    </div>
-  );
-};
-
-// This component is used to create the text paragraph
-interface TextRevealParagraphProps {
-  children?: React.ReactNode;
-  targetRef: React.MutableRefObject<HTMLDivElement | null>;
-  className?: string;
-}
-
-const TextRevealParagraph: FC<TextRevealParagraphProps> = ({
-  children,
-  targetRef,
-  className,
-}) => {
-  return (
-    <p
-      ref={targetRef}
-      className={cn(
-        "paragraph flex flex-wrap text-2xl md:text-3xl lg:text-4xl xl:text-5xl font-bold p-5 md:p-8 lg:p-10",
-        className
-      )}
-    >
-      {children}
-    </p>
-  );
-};
-
-//
-interface WordsComponentProps {
-  words: string[];
-  scrollYProgress: any;
-}
-
-const WordsComponent: FC<WordsComponentProps> = ({
-  words,
-  scrollYProgress,
-}) => {
-  return (
-    <>
-      {words.map((word, i) => {
-        const start = i / words.length;
-        const end = start + 1 / words.length;
-        return (
-          <Word key={i} progress={scrollYProgress} range={[start, end]}>
-            {word}
-          </Word>
-        );
-      })}
-    </>
-  );
-};
-
-interface TextRevealByWordComponentProps {
+interface TextRevealByWordProps {
   text: string;
+  containerClassName?: string;
+  stickyClassName?: string;
+  paragraphContainerClassName?: string;
+  paragraphClassName?: string;
+  defaultTextOpacityClassName?: string;
+  highlightedWordClassName?: string;
 }
 
-const TextRevealByWordComponent: FC<TextRevealByWordComponentProps> = ({
+export const TextRevealByWord: FC<TextRevealByWordProps> = ({
   text,
+  containerClassName,
+  stickyClassName,
+  paragraphContainerClassName,
+  paragraphClassName,
+  defaultTextOpacityClassName,
+  highlightedWordClassName,
 }) => {
   const targetRef = useRef<HTMLDivElement | null>(null);
 
   const { scrollYProgress } = useScroll({
     target: targetRef,
   });
-
   const words = text.split(" ");
 
   return (
-    <main>
-      <TextRevealHeightContainer targetRef={targetRef}>
-        <TextRevealStickyContainer>
-          <div className="max-w-4xl mx-auto px-[1rem]">
-            <TextRevealParagraph targetRef={targetRef}>
-              {words.map((word, i) => {
-                const start = i / words.length;
-                const end = start + 1 / words.length;
-                return (
-                  <Word key={i} progress={scrollYProgress} range={[start, end]}>
-                    {word}
-                  </Word>
-                );
-              })}
-            </TextRevealParagraph>
-          </div>
-        </TextRevealStickyContainer>
-      </TextRevealHeightContainer>
-    </main>
+    <div
+      ref={targetRef}
+      className={cn("h-[200vh] relative z-0", containerClassName)}
+    >
+      <div
+        className={cn(
+          "sticky top-0 py-[5rem] px-0 flex items-center h-[50%]  bg-neutral-900",
+          stickyClassName
+        )}
+      >
+        <div
+          className={cn(
+            "max-w-4xl mx-auto px-[1rem]",
+            paragraphContainerClassName
+          )}
+        >
+          <p
+            ref={targetRef}
+            className={cn(
+              "paragraph flex flex-wrap text-2xl md:text-3xl lg:text-4xl xl:text-5xl font-bold p-5 md:p-8 lg:p-10 text-white/20",
+              paragraphClassName
+            )}
+          >
+            {words.map((word, i) => {
+              const start = i / words.length;
+              const end = start + 1 / words.length;
+              return (
+                <Word
+                  key={i}
+                  progress={scrollYProgress}
+                  range={[start, end]}
+                  defaultTextOpacityClassName={defaultTextOpacityClassName}
+                  highlightedWordClassName={highlightedWordClassName}
+                >
+                  {word}
+                </Word>
+              );
+            })}
+          </p>
+        </div>
+      </div>
+    </div>
   );
-};
-
-export default TextRevealByWordComponent;
-export {
-  TextRevealHeightContainer,
-  TextRevealStickyContainer,
-  TextRevealParagraph,
-  WordsComponent,
 };
 
 interface WordProps {
   children: ReactNode;
   progress: any;
   range: [number, number];
+  defaultTextOpacityClassName?: string;
+  highlightedWordClassName?: string;
 }
-
-const Word: FC<WordProps> = ({ children, progress, range }) => {
-  const opacity = useTransform(progress, range, [0.2, 1]);
+const Word: FC<WordProps> = ({
+  children,
+  progress,
+  range,
+  defaultTextOpacityClassName,
+  highlightedWordClassName,
+}) => {
+  const opacity = useTransform(progress, range, [0, 1]);
   return (
     <span className="word relative mx-1 lg:mx-2.5 xl:lg-3">
-      <span className="shadow absolute opacity-30">{children}</span>
-      <motion.span style={{ opacity: opacity }} className="text-white">
+      <span className={cn("absolute opacity-30", defaultTextOpacityClassName)}>
+        {children}
+      </span>
+      <motion.span
+        style={{ opacity: opacity }}
+        className={cn("text-white", highlightedWordClassName)}
+      >
         {children}
       </motion.span>
     </span>
   );
 };
+
+export default TextRevealByWord;
