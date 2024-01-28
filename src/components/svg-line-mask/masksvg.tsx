42fbf0fdc2dda4efa8698f9df4b39693b50bf545
Arghya Das <arghyadasproject@gmail.com>
2024-01-28 19:49:38 +0530

    position correct for mobile screen

--- src/components/svg-line-mask/masksvg.tsx
+++ src/components/svg-line-mask/masksvg.tsx
@@ -2,7 +2,7 @@ import "../svg-line-mask/mask.css";
 
 export default function MaskSvg() {
   return (
-    <section className="h-[800px] flex items-center justify-center p-10">
+    <section className="h-[800px] flex items-center justify-center p-10 overflow-hidden">
       <div className="md:block rotate-90 md:rotate-0">
         <div className="bg-[url('../../../integrate.svg')] h-[800px] w-[500px] bg-contain bg-center bg-no-repeat relative">
           <div className="wrapper absolute top-[265px] left-[65px]">
